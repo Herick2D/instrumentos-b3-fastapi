2a033a3dd46770e6eb4697807cf3e174b8d4bc56
Herick2D <contato.herickmoreira@gmail.com>
2025-09-10 15:04:19 -0300

    FEAT: auth

--- api/auth.py
+++ api/auth.py
@@ -1,1 +1,18 @@
  
+from fastapi import Security, HTTPException, status
+from fastapi.security import APIKeyHeader
+from config import get_settings
+
+API_KEY_NAME = "X-API-KEY"
+api_key_header = APIKeyHeader(name=API_KEY_NAME, auto_error=False)
+
+settings = get_settings()
+
+async def get_api_key(api_key: str = Security(api_key_header)):
+    if api_key == settings.API_KEY:
+        return api_key
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Falha na validação das credenciais",
+        )
