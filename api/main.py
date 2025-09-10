5d74b09a7b86620f75135813b93a009cc7f4b00a
Herick2D <contato.herickmoreira@gmail.com>
2025-09-10 15:23:30 -0300

    FEAT: main

--- api/main.py
+++ api/main.py
@@ -1,1 +1,16 @@
- 
+from fastapi import FastAPI
+from api.routers import upload, history, content
+
+app = FastAPI(
+    title="Instrumentos B3 API",
+    description="API para upload e consulta de arquivos de dados financeiros.",
+    version="1.0.0"
+)
+
+app.include_router(upload.router, prefix="/api", tags=["Upload"])
+app.include_router(history.router, prefix="/api", tags=["History"])
+app.include_router(content.router, prefix="/api", tags=["Content"])
+
+@app.get("/")
+def read_root():
+    return {"message": "Bem vindo!"}
